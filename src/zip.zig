6ac4285b8993de4f69c767ce2961bcf64f5832e1
Paul Hariel <paul.hariel@outlook.com>
2024-04-10 19:53:35 +0200

    implement functions up to 0x8nnn

--- src/zip.zig
+++ src/zip.zig
@@ -1,6 +1,7 @@
 pub const ZipError = error{
     IllegalReturn,
     StackFull,
+    UnknownOp,
 };
 
 pub const Zip = struct {
@@ -27,6 +28,54 @@ pub const Zip = struct {
         self.program_counter = address;
     }
 
+    fn registerMinusRegister(self: Zip, x: u3, y: u4) void {
+        if (self.registers[x] >= self.registers[y])
+            self.registers[0xf] = 1;
+
+        self.registers[x] -= self.registers[y];
+    }
+
+    fn registerPlusRegister(self: Zip, x: u4, y: u4) void {
+        self.registers[x] += self.registers[y];
+    }
+
+    fn registerAddValue(self: Zip, register: u4, value: u8) void {
+        const result: u8 = (self.registers[register] + value) % 0x100;
+
+        self.registers[register] = result;
+    }
+
+    fn registerAndRegister(self: Zip, x: u4, y: u4) void {
+        self.registers[x] &= self.registers[y];
+    }
+
+    fn registerRegisterMinus(self: Zip, x: u4, y: u4) void {
+        if (self.registers[y] >= self.registers[x])
+            self.registers[0xf] = 1;
+
+        self.registers[x] = self.registers[y] - self.registers[x];
+    }
+
+    fn registerShiftLeft(self: Zip, register: u4) void {
+        self.registers[0xf] = (self.registers[register] & 0b1000_0000) >> 7;
+
+        self.registers[register] <<= 1;
+    }
+
+    fn registerShiftRight(self: Zip, register: u4) void {
+        self.registers[0xf] = self.registers[register] & 1;
+
+        self.registers[register] >>= 1;
+    }
+
+    fn registerOrRegister(self: Zip, x: u4, y: u4) void {
+        self.registers[x] |= self.registers[y];
+    }
+
+    fn registerXorRegister(self: Zip, x: u4, y: u4) void {
+        self.registers[x] ^= self.registers[y];
+    }
+
     fn returnFromSubroutine(self: Zip) !void {
         if (self.stack_ptr == 0) return ZipError.IllegalReturn;
 
@@ -34,20 +83,58 @@ pub const Zip = struct {
         self.stack_ptr -= 1;
     }
 
+    fn setRegisterToRegister(self: Zip, x: u4, y: u4) void {
+        self.registers[x] = self.registers[y];
+    }
+
+    fn setRegisterToValue(self: Zip, register: u4, value: u8) void {
+        self.registers[register] = value;
+    }
+
     fn skipEqual(self: Zip, register: u4, value: u8) void {
         if (self.registers[register] == value)
             self.program_counter += 1;
     }
 
+    fn skipNotEqual(self: Zip, register: u4, value: u8) void {
+        if (self.registers[register] != value)
+            self.program_counter += 1;
+    }
+
+    fn skipRegistersEqual(self: Zip, x: u4, y: u4) void {
+        if (self.register[x] == self.registers[y])
+            self.program_counter += 1;
+    }
+
     fn executeInstruction(self: Zip, instruction: u16) !void {
         switch (instruction & 0xf000) {
-            0 => switch (instruction) {
+            0x0000 => switch (instruction) {
                 0x00e0 => self.clearScreen(),
                 0x0ee0 => self.returnFromSubroutine(),
+                _ => return ZipError.UnknownOp,
             },
-            1 => self.gotoAddress(instruction & 0x0fff),
-            2 => self.callSubroutine(instruction & 0x0fff),
-            3 => self.skipEqual((instruction & 0x0f00) >> 16, instruction & 0xff),
+            0x1000 => self.gotoAddress(instruction & 0x0fff),
+            0x2000 => self.callSubroutine(instruction & 0x0fff),
+            0x3000 => self.skipEqual((instruction & 0x0f00) >> 16, instruction & 0xff),
+            0x4000 => self.skipNotEqual((instruction & 0x0f00) >> 16, instruction & 0xff),
+            0x5000 => switch (instruction & 0x000f) {
+                0 => self.skipRegistersEqual((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                _ => return ZipError.UnknownOp,
+            },
+            0x6000 => self.setRegisterToValue((instruction & 0x0f00) >> 16, instruction & 0x00ff),
+            0x7000 => self.registerAddValue((instruction & 0x0f00) >> 16, instruction & 0x00ff),
+            0x8000 => switch (instruction & 0x000f) {
+                0 => self.setRegisterToRegister((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                1 => self.registerOrRegister((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                2 => self.registerAndRegister((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                3 => self.registerXorRegister((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                4 => self.registerPlusRegister((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                5 => self.registerMinusRegister((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                6 => self.registerShiftRight((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                7 => self.registerRegisterMinus((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                14 => self.registerShiftLeft((instruction & 0x0f00) >> 16, (instruction & 0x00f0) >> 8),
+                _ => return ZipError.UnknownOp,
+            },
         }
     }
 };
