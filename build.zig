51badcb0ca825dfe04f3946e20d085c302fb0f10
Paul Hariel <paul.hariel@outlook.com>
2025-01-16 20:11:18 +0800

    Add raylib dependency

--- build.zig
+++ build.zig
@@ -1,12 +1,28 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
     const exe = b.addExecutable(.{
         .name = "zip",
-        .optimize = b.standardOptimizeOption(.{}),
+        .optimize = optimize,
         .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
+        .target = target,
     });
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 }
